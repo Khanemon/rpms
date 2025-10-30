9d933c3dd9d01a338e8de8d9026524f4a0b9180a
Emon Khan <mehediemonkhan@gmail.com>
2025-10-30 21:25:33 +0600

    Initial commit - add full project

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+interface Props{
+    children:React.ReactNode;
+    className?: string;
+}
+
+const Container = ({children, className}: Props) => {
+  return (
+    <div className={cn("max-w-screen-xl mx-auto px-4 bg-blue-500",className)}>{children}</div>
+  )
+}
+
+export default Container
